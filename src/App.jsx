02d4b8a25374fb59ed32cf909e26faf2e11e77d5
Sayeed Munees <sayeedmunees0@gmail.com>
2025-09-15 09:33:01 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Article, Brand, CTA } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <Article/>
+      <h1>GPT-3</h1>
+    </div>
+  );
+};
+
+export default App;
